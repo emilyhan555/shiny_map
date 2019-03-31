5d15f0f92e9e8a5808fb6669d30dc4a7db3fb985
Emily Han <46552109+emilyhan555@users.noreply.github.com>
2019-03-31 17:05:01 -0500

    Update shiny_map.R

--- shiny_map.R
+++ shiny_map.R
@@ -89,24 +89,8 @@ server <- function(input,output){
   
     output$AccidentTable <- DT::renderDataTable({
       req(input$x, input$y, input$z)
-      data = accident1.df[,c(1,2,4,7,8,11,12)]
-      
-      if (input$x == "FALSE") {
-        data <- data[data$Fatality == 'FALSE',]
-      } else {
-        data <- data[data$Fatality == 'TRUE',]
-      }
-      
-      if (input$y == 'FALSE') {
-        data <- data[data$Hit.And.Run == 'FALSE',]
-      } else {
-        data <- data[data$Hit.And.Run == 'TRUE',]
-      }
-      
-      if (input$z) {
-        data <- data[data$Date == input$z,]
-      }
-      
+      data = dat()[,c(1,2,4,7,8,11,12)]
+     
       DT::datatable(data, 
                     options = list(pageLength = 10), 
                     rownames = FALSE)
